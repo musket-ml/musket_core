358a0aa48d4d72b61af7ae9cf49690836605fb0b
Petrochenko Pavel <petrochenko.pavel.a@gmail.com>
2019-03-28 00:01:06 +0700

    adding generic launch script.

---
+++ musket_core/launch.py
@@ -0,0 +1,18 @@
+import sys
+from musket_core import tools,projects
+import yaml
+import threading
+from musket_core import utils
+config = sys.argv[1]
+out = sys.argv[2]
+def main():
+        global config
+        with open(config,"r") as f:
+            config=f.read()
+        config = config[1:].replace("!!com.onpositive", "!com.onpositive")
+        obj = yaml.load(config)
+        print(obj)
+        results = obj.perform(projects.Workspace(), tools.ProgressMonitor())
+        with open(out,"w") as f:
+            f.write(yaml.dump(results))
+main()
