d05e35d18d91f42903b1a19d9847e8c518096af1
ksviridov <ksviridov@gmail.com>
2019-04-05 23:04:53 +0700

    multiinput cache

--- musket_core/caches.py
+++ musket_core/caches.py
@@ -1,4 +1,6 @@
 import os
+import sys
+
 from musket_core.datasets import PredictionItem
 from musket_core.utils import load,save,readArray,dumpArray
 import tqdm
@@ -41,6 +43,7 @@ class CachedPredictionItem(PredictionItem):
     def rootItem(self):
         return self.original().rootItem()
 
+
 class DiskCache:
 
     def __init__(self,parent,items):
@@ -60,6 +63,29 @@ class DiskCache:
     def __len__(self):
         return len(self.parent)
 
+class DiskCache1:
+
+    def __init__(self,parent,items,xIsListOrTuple,yIsListOrTuple):
+        self.parent=parent
+        self.items=items
+        self.xIsListOrTuple = xIsListOrTuple
+        self.yIsListOrTuple = yIsListOrTuple
+        if hasattr(parent,"folds"):
+            self.folds=getattr(parent,"folds");
+        if hasattr(parent, "holdoutArr"):
+            self.holdoutArr = getattr(parent,"holdoutArr");
+        if hasattr(parent, "name"):
+            self.origName = getattr(parent,"name");
+
+
+    def __getitem__(self, item):
+        x = self.items[0][item] if not self.xIsListOrTuple else [self.items[0][c][item] for c in range(len(self.items[0]))]
+        y = self.items[1][item] if not self.yIsListOrTuple else [self.items[1][c][item] for c in range(len(self.items[1]))]
+        return CachedPredictionItem(item,x,y,self.parent)
+
+    def __len__(self):
+        return len(self.parent)
+
 def cache(layers,declarations,config,outputs,linputs,pName,withArgs):
     def ccc(input):
         return Cache(input)
@@ -67,6 +93,218 @@ def cache(layers,declarations,config,outputs,linputs,pName,withArgs):
     return ccc
 
 CACHE_DIR=""
+def diskcache1(layers,declarations,config,outputs,linputs,pName,withArgs):
+    def ccc(input):
+        global CACHE_DIR
+        __lock__.acquire()
+        try:
+            name = "data"
+            id = "dataset"
+
+            if hasattr(input, "name"):
+                id = getattr(input, "name")
+                name = id.replace("{", "").replace("[", "").replace("/", "").replace("\\", "").replace("]", "").replace("}", "").replace(" ", "").replace(",","").replace("\'","").replace(":", "")
+            name=CACHE_DIR+name
+            if name in storage:
+                return storage[name]
+
+            data = None
+            xStructPath = f"{name}/x.struct"
+            yStructPath = f"{name}/y.struct"
+            blocksCountPath = f"{name}/blocks_count.int"
+            if os.path.exists(xStructPath) and os.path.exists(yStructPath) and os.path.exists(blocksCountPath):
+                blocksCount = load(blocksCountPath)
+                xStruct = load(xStructPath)
+                yStruct = load(yStructPath)
+                xIsListOrTuple = xStruct[2] in ["list", "tuple"]
+                yIsListOrTuple = yStruct[2] in ["list", "tuple"]
+
+                xData, yData = init_buffers(xStruct, yStruct)
+
+                for blockInd in tqdm.tqdm(range(blocksCount), "loading disk cache for:" + id):
+                    if not xIsListOrTuple:
+                        blockPath = f"{name}/x_{blockInd}.dscache"
+                        if os.path.exists(blockPath):
+                            xBuff = load(blockPath)
+                            for x in xBuff:
+                                xData.append(x)
+                        else:
+                            raise Exception(f"Cache block is missing: {name}")
+                    else:
+                        for c in range(len(xStruct[0])):
+                            blockPath = f"{name}/x_{blockInd}_{c}.dscache"
+                            if os.path.exists(blockPath):
+                                xBuff = load(blockPath)
+                                for x in xBuff:
+                                    xData[c].append(x)
+                            else:
+                                raise Exception(f"Cache block is missing: {name}")
+                    if not yIsListOrTuple:
+                        blockPath = f"{name}/y_{blockInd}.dscache"
+                        if os.path.exists(blockPath):
+                            yBuff = load(blockPath)
+                            for y in yBuff:
+                                yData.append(y)
+                        else:
+                            raise Exception(f"Cache block is missing: {name}")
+                    else:
+                        for c in range(len(yStruct[0])):
+                            blockPath = f"{name}/y_{blockInd}_{c}.dscache"
+                            if os.path.exists(blockPath):
+                                yBuff = load(blockPath)
+                                for y in yBuff:
+                                    yData[c].append(y)
+                            else:
+                                raise Exception(f"Cache block is missing: {name}")
+
+                data = (xData, yData)
+
+            if data is None:
+                if not os.path.isdir(name):
+                    os.mkdir(name)
+
+                i0 = input[0]
+                i0x = i0.x
+                i0y = i0.y
+                l = len(input)
+
+                xStruct = inspect_structure(i0x)
+                yStruct = inspect_structure(i0y)
+
+                xIsListOrTuple = xStruct[2] in ["list", "tuple"]
+                yIsListOrTuple = yStruct[2] in ["list", "tuple"]
+
+                xData, yData = init_buffers(xStruct, yStruct)
+
+                buffSize = 0
+
+                barrier = 64 * 1024 * 1024
+
+                blockInd = 0
+                for i in tqdm.tqdm(range(l), "building disk cache for:" + id):
+                    item = input[i]
+                    if not xIsListOrTuple:
+                        xData.append(item.x)
+                    else:
+                        for c in range(len(xStruct[0])):
+                            xData[c].append(item.x[c])
+                    if not yIsListOrTuple:
+                        yData.append(item.y)
+                    else:
+                        for c in range(len(yStruct[0])):
+                            yData[c].append(item.y[c])
+
+                    buffSize += get_size(item.x)
+                    buffSize += get_size(item.y)
+
+                    if buffSize > barrier or i == l-1:
+
+                        if not xIsListOrTuple:
+                            arr = xData
+                            if xStruct[0][0].startswith("int") or xStruct[0][0].startswith("float"):
+                                arr = np.array(arr)
+                            save(f"{name}/x_{blockInd}.dscache", arr)
+                        else:
+                            for c in range(len(xStruct[0])):
+                                arr = xData[c]
+                                if xStruct[0][c].startswith("int") or xStruct[0][c].startswith("float"):
+                                    arr = np.array(arr)
+                                save(f"{name}/x_{blockInd}_{c}.dscache", arr)
+
+                        if not yIsListOrTuple:
+                            arr = yData
+                            if yStruct[0][0].startswith("int") or yStruct[0][0].startswith("float"):
+                                arr = np.array(arr)
+                            save(f"{name}/y_{blockInd}.dscache", arr)
+                        else:
+                            for c in range(len(yStruct[0])):
+                                arr = yData[c]
+                                if yStruct[0][c].startswith("int") or yStruct[0][c].startswith("float"):
+                                    arr = np.array(arr)
+                                save(f"{name}/y_{blockInd}_{c}.dscache", arr)
+
+                        buffSize = 0
+                        blockInd += 1
+                        xData, yData = init_buffers(xStruct, yStruct)
+                        pass
+
+                save(xStructPath, xStruct)
+                save(yStructPath, yStruct)
+                save(blocksCountPath, blockInd)
+                return input
+
+            result = DiskCache(input, data, xIsListOrTuple, yIsListOrTuple)
+            storage[name] = result
+            return result
+        finally:
+            __lock__.release()
+
+    return ccc
+
+
+def get_size(obj) -> int:
+    if isinstance(obj, list):
+        sum = 0
+        for x in obj:
+            sum += get_size(x)
+        return sum
+    elif isinstance(obj, tuple):
+        sum = 0
+        for x in list(obj):
+            sum += get_size(x)
+        return sum
+    elif isinstance(obj, np.ndarray):
+        return obj.size * obj.dtype.itemsize
+    else:
+        return sys.getsizeof(obj)
+
+
+def init_buffers(xStruct, yStruct):
+    xIsListOrTuple = xStruct[2] in ["list", "tuple"]
+    yIsListOrTuple = yStruct[2] in ["list", "tuple"]
+    xData = []
+    if xIsListOrTuple:
+        for i in range(len(xStruct[0])):
+            xData.append([])
+    yData = []
+    if yIsListOrTuple:
+        for i in range(len(yStruct[0])):
+            yData.append([])
+    return xData, yData
+
+def inspect_structure(obj)->([str], [[int]], str):
+    container = None
+    isTuple = isinstance(obj, tuple)
+    if isTuple:
+        container = 'tuple'
+    isList = isinstance(obj, list)
+    if isList:
+        container = 'list'
+
+    types = []
+    shapes = []
+    if isTuple:
+        for ch in list(obj):
+            chStruct = inspect_structure(ch)
+            types.extend(chStruct[0])
+            shapes.extend(chStruct[1])
+    elif isList:
+        for ch in obj:
+            chStruct = inspect_structure(ch)
+            types.extend(chStruct[0])
+            shapes.extend(chStruct[1])
+    elif isinstance(obj,np.ndarray):
+        tstr = str(obj.dtype)
+        types.append(tstr)
+        shapes.append(list(obj.shape))
+        pass
+    elif isinstance(obj, str):
+        types.append('str')
+        shapes.append([])
+        pass
+
+    return types, shapes, container
+
 def diskcache(layers,declarations,config,outputs,linputs,pName,withArgs):
     def ccc(input):
         global CACHE_DIR
