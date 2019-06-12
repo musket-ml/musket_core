661bd70f6a0ebfcf9d2a0588b8a483a02dea204b
Petrochenko Pavel <petrochenko.pavel.a@gmail.com>
2019-06-12 10:56:27 +0700

    context updates

--- musket_core/context.py
+++ musket_core/context.py
@@ -11,6 +11,16 @@ def _find_path():
     for frm in st:        
         file=frm.filename;        
         dn=os.path.dirname(file)
+        while len(dn)>0 :
+            if os.path.exists(os.path.join(dn,"modules")):
+                return dn
+            old=dn
+            dn=os.path.dirname(dn)
+            if old==dn:
+                break 
+    for frm in st:        
+        file=frm.filename;        
+        dn=os.path.dirname(file)
         if last==0:
             last=last+1
             continue
@@ -18,23 +28,13 @@ def _find_path():
             return os.path.dirname(dn)  
         if os.path.basename(file)=="projects.py" and "musket_core" in dn:
             last=0
-    for frm in st:        
-        file=frm.filename;        
-        dn=os.path.dirname(file)
-        while len(dn)>0 :
-            if os.path.exists(os.path.join(dn,"modules")):
-                return dn
-            old=dn
-            dn=os.path.dirname(dn)
-            if old==dn:
-                break  
+     
     return None            
         
 
 def get_current_project_path():
-    return _find_path()
-#     if not hasattr(context,"projectPath") and False:
-#         context.projectPath=_find_path();
-#     return context.projectPath
+    if not hasattr(context,"projectPath"):
+        context.projectPath=_find_path();
+    return context.projectPath
 
 
