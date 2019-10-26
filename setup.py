bc6dbbedca4bbaecdc452733cc14d163518eb566
Petrochenko Pavel <petrochenko.pavel.a@gmail.com>
2019-10-26 18:44:49 +0700

    next version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 import setuptools
 setup(name='musket_core',
-      version='0.49',
+      version='0.491',
       description='Common parts of my pipelines',
       url='https://github.com/petrochenko-pavel-a/musket_core',
       author='Petrochenko Pavel',
