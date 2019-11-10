f1ada763f1d50d534a7864c4756eb9e543babd87
Petrochenko Pavel <petrochenko.pavel.a@gmail.com>
2019-11-10 16:22:11 +0700

    updating package version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 import setuptools
 setup(name='musket_core',
-      version='0.492',
+      version='0.493',
       description='Common parts of my pipelines',
       url='https://github.com/petrochenko-pavel-a/musket_core',
       author='Petrochenko Pavel',
