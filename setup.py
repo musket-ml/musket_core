6bca16a09d3d558e284747827159476525c521fd
Petrochenko Pavel <petrochenko.pavel.a@gmail.com>
2019-11-14 01:40:32 +0700

    increasing version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 import setuptools
 setup(name='musket_core',
-      version='0.495',
+      version='0.496',
       description='Common parts of my pipelines',
       url='https://github.com/petrochenko-pavel-a/musket_core',
       author='Petrochenko Pavel',
