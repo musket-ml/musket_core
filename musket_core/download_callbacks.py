8285f425e89869cc068e97d8b169673da010ab5d
Dmitry Karpenko <dmitry@onpositive.com>
2020-01-16 20:37:44 +0700

    1. Fit - support one process per each experiment 2. After fit callback decorators
    Change-Id: I2ab3ca3f3210b71c9ca9e130d768fc3a1e2a7046

--- musket_core/download_callbacks.py
+++ musket_core/download_callbacks.py
@@ -6,6 +6,7 @@ after_download_callbacks=[]
 
 def after_download(method):
     after_download_callbacks.append(method)
+    return method
     pass
 
 def get_after_download_callbacks():
--- musket_core/fit.py
+++ musket_core/fit.py
@@ -1,5 +1,6 @@
 import sys
 import os
+from musket_core.fit_callbacks import after_fit_callbacks
 print("Adding " + os.path.dirname(sys.path[0]))
 #sys.path.append(os.path.dirname(sys.path[0]))
 sys.path[0] = os.path.dirname(sys.path[0])
@@ -9,9 +10,10 @@ os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
 os.environ["CUDA_VISIBLE_DEVICES"] = "0" #,1,2"
 import argparse
 from musket_core.projects import Workspace
-from musket_core import caches, deps_download
+from musket_core import caches, deps_download, fit_callbacks
 from musket_core.tools import Launch,ProgressMonitor
 import tensorflow as tf
+from multiprocessing import Process
 
 try:
     #do not remove
@@ -43,17 +45,18 @@ def main():
                         help='cache directory')
     parser.add_argument('--folds', type=str, default=None,
                         help='folds to execute')
-
     parser.add_argument('--time', type=int, default=-1,
                         help='time to work')
+    parser.add_argument('--one_process', type=bool, default=False,
+                        help='Start all experiments in one process. Good to use for several small experiments')
     parser.add_argument('-d','--download_deps', action='store_true',
                     help='download dependencies (e.g. dataset) prior to fitting')
 
     args = parser.parse_args()
     if len(args.cache)>0:
         caches.CACHE_DIR = args.cache
-    w=Workspace()
-    project=w.project(args.project)
+    workspace=Workspace()
+    project=workspace.project(args.project)
 
     experiments=project.experiments()
 
@@ -79,10 +82,26 @@ def main():
         
     if args.download_deps:
         deps_download.main(sys.argv)
-
-    l=Launch(args.gpus_per_net,args.num_gpus,args.num_workers,[x.path for x in experiments],args.allow_resume,args.only_report,args.launch_tasks, folds, args.time)
-    l.perform(w,ProgressMonitor())
+        
+    if args.one_process or len(experiments) == 1:
+        perform_experiments(args.gpus_per_net,args.num_gpus,args.num_workers,[x.path for x in experiments],args.allow_resume,args.only_report,args.launch_tasks, folds, args.time)
+    else:
+        for x in experiments:
+            p = Process(target=perform_experiments, args=(workspace,args.gpus_per_net,args.num_gpus,args.num_workers,[x.path],args.allow_resume,args.only_report,args.launch_tasks, folds, args.time))           
+            p.start()
+            p.join()               
+    
+    callbacks = fit_callbacks.get_after_fit_callbacks()
+    for func in callbacks:
+        func()
+    
     exit(0)
     pass
+
+def perform_experiments(workspace, gpus_per_net,num_gpus,num_workers, experiment_paths,allow_resume,only_report,launch_tasks, folds, time):
+    l=Launch(gpus_per_net,num_gpus,num_workers,experiment_paths,allow_resume,only_report,launch_tasks, folds, time)
+    l.perform(workspace,ProgressMonitor())
+
+
 if __name__ == '__main__':
     main()
